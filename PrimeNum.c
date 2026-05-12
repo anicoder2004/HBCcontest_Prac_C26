b3407d60e4c83af472e444cdccedec838eb63cd0
Your Name <you@example.com>
2026-05-12 14:43:25 +0600

    Old Validate Func deleted

--- PrimeNum.c
+++ PrimeNum.c
@@ -4,32 +4,7 @@
 #include <stdlib.h>
 #include "validate_func.h"
 
-int get_validated_num() {
-    char buffer[100];
-    char *endptr;
-    int mark;
-
-    while (1) {
-        printf("Enter a Number: ");
-        
-        if (fgets(buffer, sizeof(buffer), stdin) == NULL) {
-            printf("Error: System failed to read input. Exiting...\n");
-            exit(EXIT_FAILURE); }
-
-        mark = strtod(buffer, &endptr);
-
-        if (endptr == buffer || (*endptr != '\n' && *endptr != '\0')) {
-            printf("Error: Invalid input. Please enter a Valid Whole Number.\n\n");
-            continue; }
-
-        break; 
-    }
-    return mark;
-}
-
 int main() {
-
-   // int num = get_validated_num();
 
     int num = validate("Enter a Number: ","Invalid input. Please enter a Valid Whole Number.", 0,0);
 
