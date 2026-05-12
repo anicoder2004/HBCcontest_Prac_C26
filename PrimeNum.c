244410526647bb0d59a6f549ebe983dc08940802
Your Name <you@example.com>
2026-05-12 13:55:13 +0600

    Prime Number Detection Complete

---
+++ PrimeNum.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <math.h>
+#include <stdlib.h>
+
+int get_validated_num() {
+    char buffer[100];
+    char *endptr;
+    int mark;
+
+    while (1) {
+        printf("Enter a Number: ");
+        
+        if (fgets(buffer, sizeof(buffer), stdin) == NULL) {
+            printf("Error: System failed to read input. Exiting...\n");
+            exit(EXIT_FAILURE); }
+
+        mark = strtod(buffer, &endptr);
+
+        if (endptr == buffer || (*endptr != '\n' && *endptr != '\0')) {
+            printf("Error: Invalid input. Please enter a Valid Whole Number.\n\n");
+            continue; }
+
+        break; 
+    }
+    return mark;
+}
+
+int main() {
+
+    int num = get_validated_num();
+    bool isPrime = true;
+
+    if (num <= 1) { isPrime = false; } 
+    else {
+        for (int i = 2; i <= sqrt(num); i++) {
+            if (num % i == 0) { isPrime = false; break; } }
+    }
+
+    if (isPrime){ printf("%d is a prime number.\n", num); }
+    else { printf("%d is not a prime number.\n", num); }
+
+    return 0;
+}
