5e60f7b86eed386d2184adffe361b2f404b27b16
Your Name <you@example.com>
2026-05-19 12:59:38 +0600

    Only PrimeNum Remains

--- PrimeNum.c
+++ PrimeNum.c
@@ -2,11 +2,37 @@
 #include <stdbool.h>
 #include <math.h>
 #include <stdlib.h>
-#include "validate_func.h"
+
+int validate_int(char *input_msg, char *error_msg, int min, int max) {
+    char buffer[100];
+    char *endptr;
+    int value;
+
+
+    while (1) {
+        printf("%s", input_msg);
+        
+        if (fgets(buffer, sizeof(buffer), stdin) == NULL) {
+                printf("Error: System failed to read input. Exiting...\n");
+                exit(EXIT_FAILURE);  }
+
+        value = (int)strtol(buffer, &endptr, 10);
+
+        if (endptr == buffer || (*endptr != '\n' && *endptr != '\0')) { printf("Error: %s\n\n", error_msg); continue; }
+
+        // Only runs if min is not equal to max
+        if (min != max) {
+            if (value < min || value > max) { printf("Error: Input must be between %d and %d.\n\n", min, max); continue; }
+        }
+
+        break; 
+    }
+    return value;
+}
 
 int main() {
 
-    int num = validate("Enter a Number: ","Invalid input. Please enter a Valid Whole Number.", 0,0);
+    int num = validate_int("Enter a Number: ","Invalid input. Please enter a Valid Whole Number.", 0,0);
 
     bool isPrime = true;
 
--- validate_func.h
+++
@@ -1,7 +0,0 @@
-#ifndef INPUT_VALIDATION_H
-#define INPUT_VALIDATION_H
-
-// This is the "signature" of your function
-int validate(char *prompt, char *error_msg, int min, int max);
-
-#endif
